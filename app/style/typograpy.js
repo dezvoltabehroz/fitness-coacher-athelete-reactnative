88e008ab5a34a734e6c9014b480edbdfb5a0ad45
Behroz Ahmed <56107022+dezvoltabehroz@users.noreply.github.com>
2021-05-10 16:54:38 +0500

    ios fixes

--- app/style/typograpy.js
+++ app/style/typograpy.js
@@ -1,7 +1,7 @@
 export const FontFamily={
     helveticaBold:"Helvetica-Bold",
     helvetica:"Helvetica",
-    helveticaLight:"helvetica-light-587ebe5a59211",
+    // helveticaLight:"helvetica-light-587ebe5a59211",
     helveticaOblique:"Helvetica-Oblique",
     helveticaObliqueBold:"Helvetica-BoldOblique",
 
