818c8fbee9363c025b9ea3acac832891d79bca91
Behroz Ahmed <56107022+dezvoltabehroz@users.noreply.github.com>
2021-05-10 17:14:24 +0500

    Update typograpy.js

--- app/style/typograpy.js
+++ app/style/typograpy.js
@@ -1,8 +1,8 @@
-export const FontFamily={
-    helveticaBold:"Helvetica-Bold",
-    helvetica:"Helvetica",
-    // helveticaLight:"helvetica-light-587ebe5a59211",
-    helveticaOblique:"Helvetica-Oblique",
-    helveticaObliqueBold:"Helvetica-BoldOblique",
-
-}
+export const FontFamily = {
+    helveticaBold: 'Helvetica-Bold',
+    helvetica: 'Helvetica',
+    helveticaLight: 'helvetica-light',
+    helveticaOblique: 'Helvetica-Oblique',
+    helveticaObliqueBold: 'Helvetica-BoldOblique',
+  };
+  
